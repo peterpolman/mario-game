dfb26d55587ed31b0f95ae26dbbe1a540c86c186
Peter <peter@thx.network>
2023-02-11 15:56:34 +0100

    wip

--- public/app.js
+++ public/app.js
@@ -2,7 +2,7 @@
 App = {
   uiWidth: 0,
   uiHeight: 0,
-  speed: 10,
+  speed: 100,
   canvas: document.getElementById('canvas'),
   players: [],
   cars: [],
@@ -23,13 +23,19 @@ App = {
 
       // Render players
       App.players[msg] = App.createPlayer();
-      App.canvas.appendChild(App.players[msg]);
-      console.log(App.players);
+      App.players[msg].blocks.forEach((block) => {
+        App.canvas.appendChild(block);
+      });
     });
 
     // Handle disconnected players
     this.socket.on('player disconnected', function(msg){
-      App.canvas.removeChild(App.players[msg]);
+      if (!App.players[msg]) return;
+
+      App.players[msg].blocks.forEach((block) => {
+        App.canvas.removeChild(block);
+      })
+      
       App.players.splice(msg, 1);
       console.log('player disconnected: ' + msg);
     });
@@ -48,31 +54,13 @@ App = {
   },
 
   move(player, direction) {
-    if (direction == 'left') {
-      var int = App.players[player].x = App.players[player].x - (1 * App.speed);
-      App.players[player].style.left = int <= 0 ? 0 : int;
-      App.players[player].style['background-position'] = '-135px -90px';
-      App.players[player].style['transform'] = 'scaleX(-1)';
-    }
-
-    if (direction == 'right') {
-      var int = App.players[player].x = App.players[player].x + (1 * App.speed);
-      App.players[player].style.left = int >= App.uiWidth ? App.uiWidth : int;
-      App.players[player].style['transform'] = 'scaleX(1)';
-      App.players[player].style['background-position'] = '-135px -90px';
-    }
-
-    if (direction == 'up') {
-      var int = App.players[player].y = App.players[player].y - (1 * App.speed);
-      App.players[player].style.top = int <= 0 ? 0 : int;
-      App.players[player].style['background-position'] = '-72px -172px';
-    }
-
-    if (direction == 'down') {
-      var int = App.players[player].y = App.players[player].y + (1 * App.speed);
-      App.players[player].style.top = int >= App.uiHeight ? App.uiHeight : int;
-      App.players[player].style['background-position'] = '-72px -10px';
-    }
+    if (
+      (App.players[player].direction === 'left' && direction === 'right') || 
+      (App.players[player].direction === 'up' && direction === 'down') ||
+      (App.players[player].direction === 'right' && direction === 'left') ||
+      (App.players[player].direction === 'down' && direction === 'up')
+    ) return;
+    App.players[player].direction = direction;
   },
 
   bindEvents: function() {
@@ -83,17 +71,7 @@ App = {
     }
   },
 
-  // Helper method for obstacle creation
-  createCoin: function() {
-    var coin = document.createElement('div');
-    coin.classList.add = 'coin';
-    this.canvas.appendChild(coin)
-
-    return coin;
-  },
-
-  // Helper method for car creation
-  createCar: function(x, y, radius) {
+  createCoin: function(x, y, radius) {
     var car = document.createElement('span');
     car.classList.add('car');
 
@@ -106,20 +84,37 @@ App = {
 
     return car;
   },
-
+  
   // Helper method for player creation
-  createPlayer: function() {
-    var player = document.createElement('div');
-    player.classList.add('player');
+  createPlayer() {
+    const player = {
+      color: Math.floor(Math.random()*16777215).toString(16),
+      blocks: []
+    }
+    const startX = this.uiWidth / 2;
+    const startY = this.uiHeight / 2;
+    const positions = [[startX, startY], [startX + 20, startY]];
 
-    player.x = this.uiWidth / 2;
-    player.y = this.uiHeight / 2;
-    player.radius = 15;
-
-    player.style.left = player.x;
-    player.style.top = player.y;
+    for (const [x,y] of positions) {
+      const block = this.addBlock(x, y);
+      player.blocks.unshift(block);
+    };
 
     return player;
+  },
+
+  addBlock: function(x, y, color) {
+    var block = document.createElement('div');
+    block.classList.add('player');
+
+    block.x = x;
+    block.y = y;
+    block.radius = 15;
+    
+    block.style.left = block.x;
+    block.style.top = block.y;
+    block.style.backgroundColor = color;
+    return block;
   },
 
   detectCollision: function (car, player) {
@@ -133,53 +128,115 @@ App = {
     }
 
   },
+  
+  updateSnakes() {
+    for (const playerId in App.players) {
+      const player = App.players[playerId];
+      const distance = 20;
+      const block = this.addBlock(player.blocks[0].x, player.blocks[0].y, player.color);
+      
+      switch(player.direction) {
+        case 'up': {
+          block.style.top = block.y = block.y - distance;
+          break
+        }
+        case 'down': {
+          block.style.top = block.y = block.y + distance;
+          break
+        }
+        case 'left':{
+          block.style.left = block.x = block.x - distance;
+          break
+        }
+        case 'right':{
+          block.style.left = block.x = block.x + distance;
+          break
+        }
+      }
+
+      player.blocks.unshift(block);
+      App.canvas.appendChild(block);
+      App.canvas.removeChild(player.blocks.pop());
+    }
+  },
 
   updateInterface: function() {
-    for (var i = 0; i < App.cars.length; i++) {
-      var car = App.cars[i];
+    for(id in App.players){
+      const player = App.players[id];
 
-      // Move the car over Y
-      car.style.top = car.y = car.y + 10;
-
-      for(id in App.players){
+      for (var i = 0; i < App.cars.length; i++) {
+        var coin = App.cars[i];
+    
         // Detect for collision
-        var collision = App.detectCollision(car, App.players[id]);
-
-        if (collision) {
-          // Remove from active list
-          var index = App.cars.indexOf(car);
-
-          if (index > -1) {
-            App.cars.splice(index, 1);
+        for (const block of player.blocks) {
+          
+          let otherBlocks = []
+          for (const playerId in App.players) {
+            const player = App.players[playerId];
+            otherBlocks = otherBlocks.concat(player.blocks);
           }
 
-          // Remove from DOM
-          App.canvas.removeChild(car);
-        }
-        else if (!collision && car.style.top >= App.uiHeight) {
-          // Remove car if out of bound
-          App.canvas.removeChild(car);
+          for (const b of otherBlocks) {
+            const playerCollision = App.detectCollision(b, block);
+            if (playerCollision) {
+              debugger
+            }
+          }
+
+          const coinCollision = App.detectCollision(coin, block);
+          if (coinCollision) {
+            // Remove from active list
+            var index = App.cars.indexOf(coin);
+
+            if (index > -1) {
+              App.cars.splice(index, 1);
+            }
+
+            // Remove from DOM
+            App.canvas.removeChild(coin);
+
+            // Add block to player
+            const block = this.addBlock(player.blocks[0].x, player.blocks[0].y, player.color);
+            App.players[id].blocks.unshift(block);
+            App.canvas.appendChild(block);
+            
+            // Speed up
+            this.speed = this.speed - 10 > 10 ? this.speed - 10 : this.speed;
+            window.clearInterval(this.interval);
+            this.interval = window.setInterval(this.updateSnakes.bind(this), this.speed);
+          }
+          
+          if (
+            block.style.top >= App.uiHeight ||
+            block.style.top < 0 ||
+            block.style.left >= App.uiWidth ||
+            block.style.left < 0
+          ) {
+            debugger
+            // App.canvas.removeChild(block);
+          }
         }
       }
     }
   },
 
-  spawnCars: function() {
+  spawnCoins: function() {
     var posX = Math.floor((Math.random() * App.uiWidth) + 1)
+    var posY = Math.floor((Math.random() * App.uiHeight) + 1)
 
     // Render a car
-    var car = App.createCar(posX, 0, 15);
-    App.cars.push(car);
-    App.canvas.appendChild(car);
+    var coin = App.createCoin(posX, posY, 15);
+    App.cars.push(coin);
+    App.canvas.appendChild(coin);
 
     return true;
   },
 
   // Game timer fires events that randomly affect driving
-  gameTimer: function () {
-    window.setInterval(this.updateInterface, 100);
-    window.setInterval(this.createCoin, 3000);
-    window.setInterval(this.spawnCars, 2000);
+  gameTimer() {
+    this.interval = window.setInterval(this.updateSnakes.bind(this), this.speed);
+    window.setInterval(this.updateInterface.bind(this), 100);
+    window.setInterval(this.spawnCoins, 1000);
 
     return true;
   }
